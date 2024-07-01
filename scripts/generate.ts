a7838c548901e284182461824a66efa2d6e982a1
Aditya Dev <adityadev564@gmail.com>
2024-07-01 20:29:28 +0530

    converted react pages to HTML to be converted to vector embeddings

---
+++ scripts/generate.ts
@@ -0,0 +1,54 @@
+import dotenv from "dotenv";
+dotenv.config({ path: ".env" });
+// Configure dotenv before other imports
+import { DocumentInterface } from "@langchain/core/documents";
+import { Redis } from "@upstash/redis";
+import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
+import { TextLoader } from "langchain/document_loaders/fs/text";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { getEmbeddingsCollection, getVectorStore } from "../src/lib/astradb";
+
+async function generateEmbeddings() {
+  await Redis.fromEnv().flushdb();
+
+  const vectorStore = await getVectorStore();
+
+  (await getEmbeddingsCollection()).deleteMany({});
+
+  const loader = new DirectoryLoader(
+    "src/app/",
+    {
+      ".tsx": (path) => new TextLoader(path),
+    },
+    true,
+  );
+
+  const docs = (await loader.load())
+    .filter((doc) => doc.metadata.source.endsWith("page.tsx"))
+    .map((doc): DocumentInterface => {
+      const url =
+        doc.metadata.source
+          .replace(/\\/g, "/")
+          .split("/src/app")[1]
+          .split("/page.")[0] || "/";
+
+      const pageContentTrimmed = doc.pageContent
+        .replace(/^import.*$/gm, "") // Remove all import statements
+        .replace(/ className=(["']).*?\1| className={.*?}/g, "") // Remove all className props
+        .replace(/^\s*[\r]/gm, "") // remove empty lines
+        .trim();
+
+      return {
+        pageContent: pageContentTrimmed,
+        metadata: { url },
+      };
+    });
+
+  const splitter = RecursiveCharacterTextSplitter.fromLanguage("html");
+
+  const splitDocs = await splitter.splitDocuments(docs);
+
+  await vectorStore.addDocuments(splitDocs);
+}
+
+generateEmbeddings();
--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,11 +1,111 @@
 import OpenAI from "openai";
 import { ChatCompletionMessageParam } from "openai/resources/index.mjs";
-import { OpenAIStream, StreamingTextResponse } from "ai";
+import {
+  LangChainStream,
+  OpenAIStream,
+  StreamingTextResponse,
+  Message as VercelChatMessage,
+} from "ai";
+import { ChatOpenAI } from "@langchain/openai";
+import {
+  ChatPromptTemplate,
+  PromptTemplate,
+  MessagesPlaceholder,
+} from "@langchain/core/prompts";
+import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
+import { getVectorStore } from "@/lib/astradb";
+import { createRetrievalChain } from "langchain/chains/retrieval";
+import { AIMessage, HumanMessage } from "@langchain/core/messages";
+import { createHistoryAwareRetriever } from "langchain/chains/history_aware_retriever";
+import { UpstashRedisCache } from "langchain/cache/upstash_redis";
+import { Redis } from "@upstash/redis";
 
 export async function POST(req: Request) {
   try {
     const body = await req.json();
     const messages = body.messages;
+
+    const chatHistory = messages
+      .slice(0, -1)
+      .map((m: VercelChatMessage) =>
+        m.role === "user"
+          ? new HumanMessage(m.content)
+          : new AIMessage(m.content),
+      );
+
+    const currentMessageContent = messages[messages.length - 1].content;
+
+    const cache = new UpstashRedisCache({
+      client: Redis.fromEnv(),
+    });
+
+    const { stream, handlers } = LangChainStream();
+    const retriever = (await getVectorStore()).asRetriever();
+
+    const chatModel = new ChatOpenAI({
+      modelName: "gpt-3.5-turbo",
+      streaming: true,
+      callbacks: [handlers],
+      verbose: true,
+      cache,
+    });
+    const rephrasingModel = new ChatOpenAI({
+      modelName: "gpt-3.5-turbo",
+      verbose: true,
+      cache,
+    });
+
+    const rephrasePrompt = ChatPromptTemplate.fromMessages([
+      new MessagesPlaceholder("chat_history"),
+      ["user", "{input}"],
+      [
+        "user",
+        "Given the above conversation, generate a search query to look up in order to get information relevant to the current question. " +
+          "Don't leave out any relevant keywords. Only return the query and no other text.",
+      ],
+    ]);
+
+    const historyAwareRetrieverChain = await createHistoryAwareRetriever({
+      llm: rephrasingModel,
+      retriever,
+      rephrasePrompt,
+    });
+
+    const prompt = ChatPromptTemplate.fromMessages([
+      [
+        "system",
+        "You are a chatbot for a personal portfolio website. You impersonate the websites owner. " +
+          "Answer the user's questions based on the below context. " +
+          "Whenever it makes sense, provide links to pages that contains more information about the topic from the given context. " +
+          "Format your messages in markdown format.\n\n" +
+          "Context:/n{context}",
+      ],
+      new MessagesPlaceholder("chat_history"),
+      ["user", "{input}"],
+    ]);
+
+    // const chain = prompt.pipe(chatModel);
+    const combineDocsChain = await createStuffDocumentsChain({
+      llm: chatModel,
+      prompt,
+      documentPrompt: PromptTemplate.fromTemplate(
+        "Page URL: {url}\n\nPage content:\n{page_content}",
+      ),
+      documentSeparator: "\n--------\n",
+    });
+
+    const retrievalChain = await createRetrievalChain({
+      combineDocsChain,
+      retriever: historyAwareRetrieverChain,
+    });
+
+    retrievalChain.invoke({
+      input: currentMessageContent,
+      chat_history: chatHistory,
+    });
+    // chain.invoke({
+    //   input: currentMessageContent,
+    // });
 
     // const openai = new OpenAI({});
 
